08f2b22ea4cccdd801ea24f06bf6af072a29f7e9
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-08-16 13:44:28 +0000

    Update workout and comment in admin file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Credits
 Extending user model: https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
 Multiselect: https://pypi.org/project/django-multiselectfield/
+Register models: https://codinggear.blog/how-to-register-model-in-django-admin/?expand_article=1&expand_article=1
--- planner/admin.py
+++ planner/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
-from .models import Workout, Exercise
+from django.apps import apps
+from .models import *
 
-admin.site.register(Workout)
-admin.site.register(Exercise)
+planner_models = apps.get_app_config('planner').get_models()
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
+    list_filter = ('created_on', 'category')
+    list_display = ('name', 'slug', 'category', 'created_on')
+    search_fields = ['name']
+
+
+# admin.site.register(Workout, WorkoutAdmin)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'workout', 'body', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('user', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+for model in planner_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
