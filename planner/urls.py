04fbaabccda0daf1586737a75f3113c9950f5293
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-08-20 13:01:12 +0000

    Add view to list workouts filtered by category

--- planner/urls.py
+++ planner/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.HomepageView, name="home"),
+    path("", views.homepage, name="home"),
     path("workouts/", views.WorkoutList.as_view(), name="workouts"),
+    path("workouts/<category_name>/", views.CategoryList.as_view(), name="category"),
     path("<slug:slug>/", views.WorkoutDetail.as_view(), name="workout_detail")
 ]
--- planner/views.py
+++ planner/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.template import RequestContext
 from .models import Workout, Category
-# from .forms import CommentForm
 
 
-def HomepageView(request):
+def homepage(request):
     workout = Workout.objects.all()[:3]
     category = Category.objects.all()
     return render(request, 'index.html',
@@ -13,17 +13,21 @@ def HomepageView(request):
 
 class WorkoutList(generic.ListView):
     model = Workout
+    category = Category.objects.all()
     queryset = Workout.objects.order_by('-created_on')
     template_name = 'workouts.html'
     paginate_by = 6
 
-    # def get(self, request, slug, *args, **kwargs):
-    #     if slug == '':
-    #         return render(request, 'workouts.html')
-    #     else:
-    #         queryset = Workout.objects.filter(category=slug)
-    #         post = get_object_or_404(queryset, slug=slug)
-    #         return render(request, 'workouts.html')
+
+class CategoryList(generic.ListView):
+    model = Workout
+    template_name = 'workouts.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        self.category = get_object_or_404(
+            Category, slug=self.kwargs['category_name'])
+        return Workout.objects.filter(category=self.category)
 
 
 class WorkoutDetail(View):
@@ -36,23 +40,11 @@ class WorkoutDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Workout.objects.all()
         workout = get_object_or_404(queryset, slug=slug)
-        # comments = post.comments.filter(approved=True).order_by("-created_on")
-        # saved = False
-        # completed = False
-        # if workout.saves.filter(id=self.request.user.id).exists():
-        #     saved = True
-        # if workout.completed.filter(id=self.request.user.id).exists():
-        #     completed = True
 
         return render(
             request,
             "workout_detail.html",
             {
                 "workout": workout,
-                # "comments": comments,
-                # "commented": False,
-                # "saved": saved,
-                # "completed": completed,
-                # "comment_form": CommentForm()
             }
         )
