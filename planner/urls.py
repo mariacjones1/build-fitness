43ba4187f60af9cb599c25eba070afbb3b8bf5ad
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-08-23 09:35:08 +0000

    Add account signup, login and logout

--- planner/urls.py
+++ planner/urls.py
@@ -1,10 +1,11 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path("", views.homepage, name="home"),
     path("workouts/", views.WorkoutList.as_view(), name="workouts"),
     path("workouts/<category_name>/", views.CategoryList.as_view(), name="category"),
-    path("<slug:slug>/", views.WorkoutDetail.as_view(), name="workout_detail")
+    path("<slug:slug>/", views.WorkoutDetail.as_view(), name="workout_detail"),
+    path('accounts/', include('allauth.urls')),
 ]
